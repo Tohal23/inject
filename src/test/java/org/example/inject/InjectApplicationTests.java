234980a05232215aaca09d4312415faec8fc2733
Anton <krivonosenko98@gmail.com>
2020-04-16 10:55:27 +0300

    Revert "Init IntegrationTest"
    This reverts commit e00c2715

--- src/test/java/org/example/inject/InjectApplicationTests.java
+++ src/test/java/org/example/inject/InjectApplicationTests.java
@@ -5,22 +5,15 @@ import org.example.inject.service.fake.FakeEmailSender;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-@AutoConfigureMockMvc
 class InjectApplicationTests {
 
 	@TestConfiguration
@@ -35,11 +28,8 @@ class InjectApplicationTests {
 	@Autowired
 	EmailSender emailSender;
 
-	@Autowired
-	private MockMvc mockMvc;
-
 	@Test
-	void contextLoads() throws Exception {
-		this.mockMvc.perform(post("/api/profiles/action")).andExpect(status().isOk());
+	void contextLoads() {
+		emailSender.send();
 	}
 }
