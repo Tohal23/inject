881e860ba1541ee6e4db5952e3d9a183f3f575aa
Anton <krivonosenko98@gmail.com>
2020-04-16 10:29:51 +0300

    Init otherInject

--- src/test/java/org/example/inject/InjectApplicationTests.java
+++ src/test/java/org/example/inject/InjectApplicationTests.java
@@ -1,21 +1,29 @@
 package org.example.inject;
 
-import org.example.inject.config.FakeEmailSenderContext;
 import org.example.inject.service.EmailSender;
+import org.example.inject.service.fake.FakeEmailSender;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-@ContextConfiguration(classes = {InjectApplication.class, FakeEmailSenderContext.class})
-@WebAppConfiguration
 class InjectApplicationTests {
+
+	@TestConfiguration
+	static class TestConfig {
+		@Bean
+		@Primary
+		public EmailSender getEmailSender() {
+			return new FakeEmailSender();
+		}
+	}
 
 	@Autowired
 	EmailSender emailSender;
